bd19be2b6283dccbce8a190b6bfd0572a7750ae4
MonsterMetroid <monstermetroid@gmail.com>
2017-05-16 16:55:58 -0700

    added functionality gifs now appear, still need to implement forms and pause start functionality

---
+++ assets/javascript/gifs.js
@@ -0,0 +1,47 @@
+var queryUrl;
+var apiKey = "dc6zaTOxFJmzC";
+var gifApp={
+	buttonDisplay: $("#buttonArea"),
+	gifDisplay: $("#gifArea"),
+	topicArray : ["Dinosaur", "Sword", "Armor", "Hacking", "Work" ],
+	createURL : function(topic){
+		queryUrl="http://api.giphy.com/v1/gifs/search?q=" +
+        topic + "&api_key="+ apiKey + "&limit=10";
+	},
+	addTopic : function(topic){
+		gifApp.topicArray.push(topic);
+	},
+	updateButtons : function(){
+		gifApp.buttonDisplay.html(null);
+		for (var i = 0; i < gifApp.topicArray.length; i++) {
+			var topic = gifApp.topicArray[i];
+			gifApp.buttonDisplay.append($("<button type='button' class='btn btn-primary' data=" + topic + ">" + topic + "</button>"));
+		}
+	}
+}
+gifApp.updateButtons();
+$("#buttonArea").on("click", "button", function(){
+	gifApp.createURL($(this).attr("data"));
+	$.ajax({
+	url: queryUrl,
+	method: "GET"
+}).done(function(response) {
+  var results = response.data;
+  console.log(response.data);
+  for (var i = 0; i < results.length; i++) {
+    var gifDiv = $("#gifArea").append($("<div class='item'>"));
+
+    var rating = results[i].rating;
+
+    var p = $("<p>").text("Rating: " + rating);
+
+    var image = $("<img>");
+    image.attr("src", results[i].images.fixed_width.url);
+    
+    gifDiv.append(p);
+    gifDiv.append(image);
+    
+    // $("#gifs-appear-here").prepend(gifDiv);
+  }
+});
+});
